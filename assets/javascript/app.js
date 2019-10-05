dc6471c3787d471bc0a5e0cd474a7cb86f1c063a
Anish Patel <anishpatel39@gmail.com>
2019-10-04 20:33:00 -0400

    Second Commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,48 +17,46 @@ $("#movieList-buttons").empty();
 
 // Button for movies entered
 $("#add-movie").on("click", function () 
-    {
-		event.preventDefault();
-		var movieButton = $("#movieButton-input").val().trim();
-		movieButton.push(movieButton);
-		renderButtons();
-		return;
-	});
+{
+	event.preventDefault();
+	var movieButton = $("#movieButton-input").val().trim();
+	movieButton.push(movieButton);
+	renderButtons();
+	return;
+});
 
 
-	// Gif API
-    $("button").on("click", function () 
-    {
-		var movie = $(this).attr("data-movie");
-		var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ds6DyfMaOz1WRfxGhI3MQlLgJVWh5XDZ&q=movie&limit=10&offset=0&rating=G&lang=en" 
+// Gif API
+$("button").on("click", function () 
+{
+	var movie = $(this).attr("data-movie");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ds6DyfMaOz1WRfxGhI3MQlLgJVWh5XDZ&q=movie&limit=10&offset=0&rating=G&lang=en" 
+	movie + "ds6DyfMaOz1WRfxGhI3MQlLgJVWh5XDZ"
 
-		movie + "ds6DyfMaOz1WRfxGhI3MQlLgJVWh5XDZ"
-
-	// Taken from ajax activity(Calling on AJAX)
-		$.ajax(
+// Taken from ajax activity(Calling on AJAX)
+	$.ajax(
 			
-        {
-		url: queryURL,
-		method: "GET"
-        }).done(function (response) 
+{
+	url: queryURL,
+	method: "GET"
+}).done(function (response) 
 
+{
+	var results = response.data;
+	$("#movies").empty();
+    for (var i = 0; i < results.length; i++) 
         {
-			var results = response.data;
-			$("#movies").empty();
-            for (var i = 0; i < results.length; i++) 
-            {
-				var gifDiv = $("<div>");
-				var p = $("<p>").text("Rating: " + results[i].rating);
-				var gifimage= $("<img>");
-
-				gifimage.attr("src", results[i].images.original_still.url);
-				gifimage.attr("data-still", results[i].images.original_still.url);
-				gifimage.attr("data-animate", results[i].images.original.url);
-				gifimage.attr("data-state", "still");
-				gifimage.attr("class", "gif");
-				gifDiv.append(gifimage);
-				$("#movies").append(gifDiv);
-			}
-		});
-	});
+			var gifDiv = $("<div>");
+			var p = $("<p>").text("Rating: " + results[i].rating);
+			var gifimage= $("<img>");
+			gifimage.attr("src", results[i].images.original_still.url);
+			gifimage.attr("data-still", results[i].images.original_still.url);
+			gifimage.attr("data-animate", results[i].images.original.url);
+			gifimage.attr("data-state", "still");
+			gifimage.attr("class", "gif");
+			gifDiv.append(gifimage);
+			$("#movies").append(gifDiv);
+		}
+});
+});
 });
